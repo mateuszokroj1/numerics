013dfc2f1ce2e5fcc4d597344fa65f2f8cc4a7fd
Mateusz Okrój <mateuszokroj1@gmail.com>
2023-08-04 01:40:52 +0200

    New files.

---
+++ primitives/prim_matrix.h
@@ -0,0 +1,7 @@
+#pragma once
+
+typedef struct
+{
+	float elements[4];
+} mat2f;
+
---
+++ primitives/prim_vector.h
@@ -0,0 +1,27 @@
+#pragma once
+
+typedef struct
+{
+	double x;
+	double y;
+} vec2d;
+
+typedef struct
+{
+	float x;
+	float y;
+} vec2f;
+
+typedef struct
+{
+	double x;
+	double y;
+	double z;
+} vec3d;
+
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+} vec3f;
